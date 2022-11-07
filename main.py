af9d033188ba086237482fc5a5f37322356c8912
Gaganjot Singh <84645711+jimmy2156@users.noreply.github.com>
2022-11-07 13:22:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from datetime import datetime as dt
+
+import time
+def get_driver():
+  options = webdriver.ChromeOptions()
+  options.add_argument("disable-infobars")
+  options.add_argument("start-maximized")
+  options.add_argument("disable-dev-shm-usage")
+  options.add_argument("no-sandbox")
+  options.add_experimental_option("excludeSwitches", ["enable-automation"])
+  options.add_argument("disable-blink-features=AutomationControlled")
+  driver = webdriver.Chrome(options = options)
+  driver.get("http://automated.pythonanywhere.com")
+  return driver
+def clean_text(text):
+  new_text = float(text.split(": ")[1])
+  return new_text
+
+
+def write_file(text):
+  filename = f"{dt.now().strftime('%Y-%m-%d.%H-%M-%S')}.txt"
+  with open(filename, "w") as file:
+    file.write(text)
+
+def main():
+  driver = get_driver()
+  while True:
+    time.sleep(2)
+    element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
+    text = str(clean_text(element.text))
+    write_file(text)
+
+
+print(main())
